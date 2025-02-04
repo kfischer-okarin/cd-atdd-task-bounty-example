5e2333a5dd05d1ef4ee22845778cb3813c834f65
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-04 23:29:12 +0900

    Create CapybaraAcceptanceTestDriver class and use as driver

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 require_relative "acceptance_test_dsl"
+require_relative "capybara_acceptance_test_driver"
 
 class ApplicationSystemTestCase < ActiveSupport::TestCase
   include AcceptanceTestDSL
+
+  setup do
+    # If we had several drivers we could use for example an environment variable to select the driver
+    # Since we only have one driver, we can hardcode it here for now
+    @driver = CapybaraAcceptanceTestDriver.new(self)
+  end
+
+  teardown do
+    @driver.teardown
+  end
 end
---
+++ test/capybara_acceptance_test_driver.rb
@@ -0,0 +1,37 @@
+require "capybara/dsl"
+require "capybara/minitest"
+
+Capybara.configure do |config|
+  # If necessary we could probably also set this via an environment variable
+  config.default_driver = :selenium_chrome_headless
+  config.run_server = false
+  config.app_host = ENV["APP_URL"]
+end
+
+class CapybaraAcceptanceTestDriver
+  class CapybaraAPI
+    include Capybara::DSL
+    include Capybara::Minitest::Assertions
+
+    # Minitest assertions assume that they are direct instance methods of the test case and can access the assertion
+    # count to increment it for the test statistics. Since this object is not a test case, we need to delegate the
+    # assertions accessor to the test case.
+    delegate :assertions, :assertions=, to: :@test_case
+
+    def initialize(test_case)
+      @test_case = test_case
+    end
+  end
+
+  def initialize(test_case)
+    # For accessing the default assertion APIs of Minitest
+    @test_case = test_case
+    # Access Capybara API through a separate object to avoid polluting the driver with Capybara methods
+    @capybara = CapybaraAPI.new(test_case)
+  end
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
