b6a9d7799b7e9b195d860195e32eeacbdfa0b92b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-10 18:35:11 +0900

    Generate a simple User model
    User authentication of any kind will be out of scope for this example so we will just use a dummy User model which will
    be directly created from a public URL.
    
    I ran
    ```
    ./bin/rails generate model User name:string
    ```
    and then deleted all test related files

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_10_093443) do
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
