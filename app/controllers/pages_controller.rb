57b41e5c547c3c82b1cf180cc5f1b75d975a1c45
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-11 10:31:50 +0900

    Add an empty Dashboard as root page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def dashboard
+    # Logic for displaying the dashboard can be added here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#dashboard"
 end
