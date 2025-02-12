33843876d114707d9962d81d0524646a2e85421b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-12 16:47:15 +0900

    Refactor ApplicationController to have access to current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,21 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  helper_method :current_user
+
   private
 
   def require_login
-    unless session[:user_id]
-      flash[:alert] = "You must be logged in to access this page."
-      redirect_to login_path
-    end
+    return if current_user
+
+    flash[:alert] = "You must be logged in to access this page."
+    redirect_to login_path
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
+    reset_session
+    nil
   end
 end
