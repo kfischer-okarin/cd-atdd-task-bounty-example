2610bc2ff1cef938e09eef72f56831f7d29e7ea4
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-15 10:55:36 +0900

    Separate capybara setup from driver methods
    The capybara setup is independent of the actual application and just noise when writing new driver methods

--- test/support/capybara_acceptance_test_driver.rb
+++ test/support/capybara_acceptance_test_driver.rb
@@ -1,29 +1,6 @@
-require "action_dispatch/system_testing/test_helpers/screenshot_helper"
-require "capybara/dsl"
-require "capybara/minitest"
-
-Capybara.configure do |config|
-  # If necessary we could probably also set this via an environment variable
-  config.default_driver = :selenium_chrome_headless
-  config.run_server = false
-  config.app_host = ENV["APP_URL"]
-end
+require_relative "capybara_acceptance_test_driver/capybara_api"
 
 class CapybaraAcceptanceTestDriver
-  class CapybaraAPI
-    include Capybara::DSL
-    include Capybara::Minitest::Assertions
-    include ActionDispatch::SystemTesting::TestHelpers::ScreenshotHelper
-
-    # Minitest assertions and screenshot helper assume the class they are included in is a test case and need to
-    # access the test case's methods, so we delegate them to the test case
-    delegate_missing_to :@test_case
-
-    def initialize(test_case)
-      @test_case = test_case
-    end
-  end
-
   def initialize(test_case)
     # For accessing the default assertion APIs of Minitest
     @test_case = test_case
