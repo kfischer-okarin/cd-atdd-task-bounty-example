c5e4761195ccf4f1df477c9ed6c56b47a191db31
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-15 10:31:22 +0900

    Move driver setup/teardown to outermost hooks
    So that testcases can define their own setup/teardown hooks without interfering with the final driver

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,14 +7,14 @@ require_relative "id_alias_map"
 class ApplicationSystemTestCase < ActiveSupport::TestCase
   include AcceptanceTestDSL
 
-  setup do
+  def before_setup
     # If we had several drivers we could use for example an environment variable to select the driver
     # Since we only have one driver, we can hardcode it here for now
     @driver = CapybaraAcceptanceTestDriver.new(self)
     @id_alias_map = IdAliasMap.new
   end
 
-  teardown do
+  def after_teardown
     @driver.teardown
   end
 end
