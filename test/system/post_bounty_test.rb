039d16186726c97bb5c8931d3cd4ba14a103d18a
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-17 01:37:37 +0900

    Add failing test

--- test/system/post_bounty_test.rb
+++ test/system/post_bounty_test.rb
@@ -13,4 +13,12 @@ class PostBountyTest < ApplicationSystemTestCase
       should_see_open_task "Fix the login page", reward: 40, posted_by: "Bob"
     end
   end
+
+  test "WIP: Users need enough balance to post a bounty" do
+    given_a_user "Bob", balance: 30
+
+    as "Bob" do
+      cannot_post_bounty "A too expensive bounty", reward: 40, because: "insufficient balance"
+    end
+  end
 end
